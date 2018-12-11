7cac766918056e66af068c6114acbad674a4b135
Thomas Hansen <ctrhansen@yahoo.com>
2018-12-11 17:00:19 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,13 @@
+var recipes = {prop: 1, prop2: 2}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return 
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+}
+function deleteFromObjectByKey(object, key) {
+  
+}
+function destructivelyDeleteFromObjectByKey(object, key) {
+  
+}
