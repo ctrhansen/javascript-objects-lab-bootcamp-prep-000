cb47e7b91ab923274e1d08cbf7d5f5fa289685d3
Thomas Hansen <ctrhansen@yahoo.com>
2018-12-11 18:34:30 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,19 @@
+var recipes = {prop: 1, prop2: 2}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return {prop: 1, prop2: 2}
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return {prop: 1, prop2: 2}
+}
+function deleteFromObjectByKey(object, key) {
+  var obj = { prop: 1 }
+  var newObj = Object.assign({}, object)
+  delete newObj.foo 
+  return {prop: 1}
+}
+function destructivelyDeleteFromObjectByKey(object, key) {
+  
+}
+ 
